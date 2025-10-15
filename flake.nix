6e0485d4fd472766ce0636912f6c6953500c4c99
exheze <exheze@dbc.dk>
2025-10-15 18:59:05 +0200

    added auto start of postgres

--- flake.nix
+++ flake.nix
@@ -132,27 +132,28 @@
             PKG_CONFIG_PATH = nixpkgs.lib.makeSearchPath "lib/pkgconfig" [ openssl.dev ];
 
             shellHook = ''
-                            ${preCommitHook} 
-              	      
-                            export HOSTMAP_TEMPLATES_DIR='./templates'
-              	      export RUST_LOG='info,hostmap=debug'
+                                          ${preCommitHook} 
+                            	      
+                                          export HOSTMAP_TEMPLATES_DIR='./templates'
+                            	      export RUST_LOG='info,hostmap=debug'
 
-                            export PG=$PWD/.dev_postgres
-                            export PGDATA=$PG/data
-                            export PGPORT=5432
-                            export PGHOST=localhost
-                            export PGUSER=$USER
-                            export PGPASSWORD=postgres
-                            export PGDATABASE=hostmap-dev
-                            export DATABASE_URL=postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE
-                            alias pg_start="pg_ctl -D $PGDATA -l $PG/postgres.log start"
-                            alias pg_stop="pg_ctl -D $PGDATA stop"
-                            pg_initial_setup() {
-                              pg_stop;
-                              rm -rf $PG;
-                              initdb -D $PGDATA &&
-                              echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf && pg_start && createdb
-                            }
+                                          export PG=$PWD/.dev_postgres
+                                          export PGDATA=$PG/data
+                                          export PGPORT=5432
+                                          export PGHOST=localhost
+                                          export PGUSER=$USER
+                                          export PGPASSWORD=postgres
+                                          export PGDATABASE=hostmap-dev
+                                          export DATABASE_URL=postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE
+                                          alias pg_start="pg_ctl -D $PGDATA -l $PG/postgres.log start"
+                                          alias pg_stop="pg_ctl -D $PGDATA stop"
+                                          pg_initial_setup() {
+                                            pg_stop;
+                                            rm -rf $PG;
+                                            initdb -D $PGDATA &&
+                                            echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf && pg_start && createdb
+                                          }
+              			    pg_ctl -D .dev_postgres/data/ status && echo "Server already running" || pg_start
             '';
           };
       };
